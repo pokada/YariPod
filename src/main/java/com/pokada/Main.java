69c9ec2a66c045aac5ed5069b08f0c181a44a715
egusahiroaki <egusa.hiroaki@gmail.com>
2016-03-21 21:25:40 +0900

    get current user directory

--- src/main/java/com/pokada/Main.java
+++ src/main/java/com/pokada/Main.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import java.io.File;
 
 public class Main extends Application {
 
@@ -18,6 +19,35 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("FXMLサンプル");
 
+        // get current user name
+
+        String path = "/Users/";
+        String username = System.getProperty("user.name");
+        StringBuilder userCurrentPath = new StringBuilder();
+        userCurrentPath.append(path);
+        userCurrentPath.append(username);
+        File dir = new File(new String(userCurrentPath));
+
+
+
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            if (files[i].isFile()){
+                //ファイル名表示
+                System.out.println(file);
+            }else if (files[i].isDirectory()){
+                //ディレクトリ名表示(※１)
+                System.out.println(file);
+            }
+        }
+
+        
+        
+        
+        
+        
+        
         AnchorPane root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
